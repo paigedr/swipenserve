f0694fae44066e1439f2c07358eb019d167ac7f8
Mac2492 <mac2492@gmail.com>
2012-05-04 07:57:35 -0700

    images by Jessica

--- Intro.xaml.cs
+++ Intro.xaml.cs
@@ -24,7 +24,6 @@ namespace Controls
         public Intro()
         {
             InitializeComponent();
-            GRR.Source = new Uri("Images/intro.m4v", UriKind.Relative);
         }
         
     }
